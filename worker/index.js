3fac720253ec877c76a7ffd6738a1e217c8f6347
steph-harris <steph.harris21@gmail.com>
2019-01-07 20:32:22 -0500

    initial base commit of multi-container practice app

---
+++ worker/index.js
@@ -0,0 +1,20 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.client({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const sub = redisClient.duplicate();
+
+function fib(idx) {
+  if (idx < 2) return 1;
+
+  return fib(idx - 1) + fib(idx - 2);
+}
+
+sub.on('message', (channel, msg) => {
+  redisClient.hset('values', msg, fib(parseInt(msg)));
+});
+sub.subscribe('insert');
